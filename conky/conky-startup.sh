4bace65fe9ac8ab3b888b0daeb1038eb2f89a000
charbaak <dip8228@gmail.com>
2017-05-04 01:25:38 +0600

    Add files via upload

---
+++ conky/conky-startup.sh
@@ -0,0 +1,18 @@
+sleep 20s
+killall conky
+cd "/home/dip/.conky/Conky-2.1/conf"
+conky -c "/home/dip/.conky/Conky-2.1/conf/conkyrc-gvs-full-v2-1360x768" &
+cd "/home/dip/.conky/Personal1"
+conky -c "/home/dip/.conky/Personal1/conkyrc2core" &
+cd "/home/dip/.conky/Personal2"
+conky -c "/home/dip/.conky/Personal2/white" &
+cd "/home/dip/.conky/Personal3"
+conky -c "/home/dip/.conky/Personal3/Blue" &
+cd "/home/dip/.conky/Personal5/lsd"
+conky -c "/home/dip/.conky/Personal5/lsd/conkyrc" &
+cd "/home/dip/.conky/haxOS_Conky"
+conky -c "/home/dip/.conky/haxOS_Conky/.conky_cal" &
+cd "/home/dip/.conky/haxOS_Conky"
+conky -c "/home/dip/.conky/haxOS_Conky/.conky_stod" &
+cd "/home/dip/.conky/haxOS_Conky"
+conky -c "/home/dip/.conky/haxOS_Conky/.conkyrc" &
